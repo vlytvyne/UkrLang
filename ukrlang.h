d20889bdb2517c642909001e9f0472bb69c7589e
Vadym Lytvynets <v.lytvynets61@gmail.com>
2019-03-06 17:05:41 +0200

    СЛАВА УКРАЇНІ!

---
+++ ukrlang.h
@@ -0,0 +1,27 @@
+#ifndef UKR_LANG
+# define UKR_LANG
+
+#define СЛАВА_УКРАЇНІ(x) main(x)
+#define ГЕРОЯМ_СЛАВА(x) return(x)
+
+#define гривня int
+#define буква char
+#define гривня_з_копійками double
+#define ніхріна void
+#define дофіга(x) x*
+
+#define от_стільки =
+#define так_само ==
+#define більше >
+#define менше <
+#define нарощувати(x) x++
+
+#define перемога if
+#define зрада else
+#define ганяти_по_колу_доки while
+
+#define червонець 10
+#define вякати printf
+
+
+#endif
